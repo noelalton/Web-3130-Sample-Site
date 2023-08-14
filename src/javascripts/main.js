53a7905f6daa543a16752ea77a2aacec3cf9db12
Noel Alton <70001812+noelalton@users.noreply.github.com>
2023-08-14 12:50:23 -0600

    Initial commit

---
+++ src/javascripts/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import '../stylesheets/main.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+//TODO - Your ES6 JavaScript code (if any) goes here
